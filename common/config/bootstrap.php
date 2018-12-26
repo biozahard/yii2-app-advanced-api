8e70670675e423ef4e740f13a9c4c31a6d3ff42d
Biozahard <biozahard@gmail.com>
2018-12-26 21:50:40 +0200

    added dd(...$args) function

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,21 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/console');
+
+/**
+ * small helper for debugging
+ * @param array ...$args
+ */
+function dd(...$args)
+{
+    echo time() . "\n";
+    if (empty($args)) {
+    } else {
+        foreach ($args as $item) {
+            echo "<pre>";
+            var_dump($item);
+            echo "</pre>";
+        }
+    }
+    exit;
+}
